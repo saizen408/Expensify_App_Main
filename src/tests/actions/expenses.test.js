ad9422fad0d750fd043d3f46fd056c3ae1743224
Saizen408 <llkoole7@gmail.com>
2020-01-12 05:19:10 -0800

    add startEditExpense

--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -6,6 +6,7 @@ import {
   startAddExpense,
   startSetExpenses,
   startRemoveExpense,
+  startEditExpense,
   setExpenses,
   addExpense,
   editExpense,
@@ -60,6 +61,27 @@ test("should setup edit expense action object", () => {
     id: "123abc",
     updates: { description: "test check" }
   });
+});
+
+test("should edit expense from firebase", done => {
+  const store = createMockStore({});
+  const id = expenses[0].id;
+  const updates = { amount: 21589 };
+  store
+    .dispatch(startEditExpense(id, updates))
+    .then(() => {
+      const actions = store.getActions();
+      expect(actions[0]).toEqual({
+        type: "EDIT_EXPENSE",
+        id,
+        updates
+      });
+      return database.ref(`expenses/${id}`).once("value");
+    })
+    .then(snapshot => {
+      expect(snapshot.val().amount).toBe(updates.amount);
+      done();
+    });
 });
 
 test("should setup add expense action object with provided values", () => {
